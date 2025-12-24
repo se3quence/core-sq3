28738d4496440ba81c8c66b577397b3fb6daf721
Hasal Dharmagunawradana <107778591+hesxo@users.noreply.github.com>
2025-12-24 10:10:38 +0530

    Delete .next directory

--- .next/dev/static/chunks/src_app_auth_sign-in_[[___rest]]_page_tsx_de57450e._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_648a8e34._.js"
-],
-    source: "dynamic"
-});
